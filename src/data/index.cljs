40b3821dfa0d07ed678382f0b0bbef8b8909c0b9
Juan M. Uys <opyate@gmail.com>
2018-06-01 23:17:37 +0100

    data-driven

---
+++ src/data/index.cljs
@@ -0,0 +1,14 @@
+(ns data.index)
+
+(def data
+    {
+        10 {
+            :text "Notepad"
+        }
+        100 {
+            :text "Moleskine"
+        }
+        1000 {
+            :text "iPad"
+        }
+    })
