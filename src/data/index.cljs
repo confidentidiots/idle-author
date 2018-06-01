9c300e14402688e0726005b30c6ba509ec147d41
Juan M. Uys <opyate@gmail.com>
2018-06-01 23:36:08 +0100

    adding every second

--- src/data/index.cljs
+++ src/data/index.cljs
@@ -4,11 +4,14 @@
     {
         10 {
             :text "Notepad"
+            :bump 0.1
         }
         100 {
             :text "Moleskine"
+            :bump 0.5
         }
         1000 {
             :text "iPad"
+            :bump 1
         }
     })
