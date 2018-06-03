46578dfcd260e312f81189de532ebf76d5d3ad35
Juan M. Uys <opyate@gmail.com>
2018-06-03 22:26:32 +0100

    clicker

--- src/data/index.cljs
+++ src/data/index.cljs
@@ -4,14 +4,34 @@
     {
         10 {
             :text "Notepad"
-            :bump 0.1
+            :bump 1
         }
         100 {
             :text "Moleskine"
-            :bump 0.5
+            :bump 2
         }
         1000 {
             :text "iPad"
+            :bump 4
+        }
+    })
+
+(def sale
+    {
+        10 {
+            :text "Slogan"
             :bump 1
+        }
+        100 {
+            :text "Copy"
+            :bump 2
+        }
+        1000 {
+            :text "Article"
+            :bump 4
+        }
+        10000 {
+            :text "Short Story"
+            :bump 5
         }
     })
