e7d30b6b26011fa3e69936b9748ba4f3b34e7a15
Juan M. Uys <opyate@gmail.com>
2018-06-07 23:56:37 +0100

    loop

--- src/data/index.cljs
+++ src/data/index.cljs
@@ -1,5 +1,12 @@
 (ns data.index)
 
+(def multiplier (fn [n] (* n 2)))
+
+(def upgrades
+    ["Notepad", "Moleskine", "iPad", "Laptop", "Amanuensis"])
+(def products
+    ["Slogan", "Copy", "Blog post", "Magazine article", "Newspaper article", "Short story"])
+
 (def data
     {
         10 {
