a7eeb20d1ed4561c27a0295025c91e8776447809
Juan M. Uys <opyate@gmail.com>
2018-06-13 23:25:40 +0100

    numbers

--- src/data/index.cljs
+++ src/data/index.cljs
@@ -2,43 +2,21 @@
 
 (def multiplier (fn [n] (* n 2)))
 
-(def upgrades
-    ["Notepad", "Moleskine", "iPad", "Laptop", "Amanuensis"])
-(def products
-    ["Slogan", "Copy", "Blog post", "Magazine article", "Newspaper article", "Short story"])
+(def upgrades [
+    "Notepad",
+    "Moleskine",
+    "iPad",
+    "Laptop",
+    "Amanuensis",
+    "a","b","c","d","e","f"
+    ])
 
-(def data
-    {
-        10 {
-            :text "Notepad"
-            :bump 1
-        }
-        100 {
-            :text "Moleskine"
-            :bump 2
-        }
-        1000 {
-            :text "iPad"
-            :bump 4
-        }
-    })
-
-(def sale
-    {
-        10 {
-            :text "Slogan"
-            :bump 1
-        }
-        100 {
-            :text "Copy"
-            :bump 2
-        }
-        1000 {
-            :text "Article"
-            :bump 4
-        }
-        10000 {
-            :text "Short Story"
-            :bump 5
-        }
-    })
+(def products [
+    "Slogan",
+    "Copy",
+    "Blog post",
+    "Magazine article",
+    "Newspaper article",
+    "Short story",
+    "a","b","c","d","e"
+    ])
