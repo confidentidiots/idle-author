593c5dc0a56be25a111f45128b3307523632f2dd
Juan M. Uys <opyate@gmail.com>
2018-06-25 21:53:33 +0100

    move sell op out into transactional

--- src/clicker/engine.cljs
+++ src/clicker/engine.cljs
@@ -7,4 +7,10 @@
 (defn click [state]
   (swap! state update-in [:clicks] inc))
 
-; (defn sell [thing])
+(defn- sell-atomic [thing state]
+  (-> state
+      (update-in [:things thing] (fnil inc 0))))
+      
+(defn sell [state thing]
+  (swap! state (partial sell-atomic thing)))
+  
--- src/clicker/engine_test.cljs
+++ src/clicker/engine_test.cljs
@@ -1,10 +1,13 @@
 (ns clicker.engine-test
   (:require [cljs.test :refer-macros [deftest is use-fixtures]]
-            [clicker.engine :refer [click]]))
-
-
-; (use-fixtures :each (fn [f]))
+            [clicker.engine :refer [click sell]]))
 
 (deftest test-click
   (let [state (atom { :clicks 0})]
     (is (= (click state) {:clicks 1}))))
+
+;
+(deftest test-sell
+  (let [state (atom { :things {"bar" 2}})]
+    (is (= (sell state "foo") {:things { "foo" 1 "bar" 2}}))
+    (is (= (sell state "bar") {:things { "foo" 1 "bar" 3}}))))
